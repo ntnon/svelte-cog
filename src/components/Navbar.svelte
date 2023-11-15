2c025255656286e3b4363025569893d0bd333fa8
ntnon <Anton.nydal@hotmail.com>
2023-11-15 17:20:04 +0100

    create navbar component

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import routes from '$lib/routes.json';
+	import type { IRoute } from '$lib/interfaces';
+	// Create a writable store to keep track of the current path
+	const location = $page.route.id;
+
+	function isNextRoute(route: IRoute) {
+		if (location === null) return false;
+		const currentIndex = routes.findIndex((route) => route.path === location);
+		return route.id === currentIndex + 1;
+	}
+</script>
+
+<nav>
+	<div class="current-page">{location}</div>
+	<ul>
+		{#each routes as route}
+			<a href={route.path} class={isNextRoute(route) ? '' : 'hidden'}> Next</a>
+		{/each}
+	</ul>
+</nav>
+
+<style>
+	.hidden {
+		display: none;
+	}
+</style>
