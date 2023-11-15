96222e543455e03899bab0141401f5c23944ab04
ntnon <Anton.nydal@hotmail.com>
2023-11-15 17:20:40 +0100

    Update +layout.svelte

--- src/routes/tasks/+layout.svelte
+++ src/routes/tasks/+layout.svelte
@@ -1,5 +1,20 @@
-<nav>
-    <li><a href="/tasks">Next</a></li>
-</nav>
+<script lang="ts">
+	import Navbar from '../../components/Navbar.svelte';
+	import { updateProgress, getProgress } from '../../stores/progressTracker';
+
+	import type { IRoute, IStatus } from '$lib/interfaces';
+	import { setContext } from 'svelte';
+
+	const sharedUpdateProgress = (route: IRoute, status: IStatus['status']) => {
+		updateProgress(route, status);
+	};
+	const sharedGetProgress = (route: IRoute) => {
+		return getProgress(route);
+	};
+	setContext('getProgress', sharedUpdateProgress);
+	setContext('updateProgress', sharedGetProgress);
+</script>
+
+<Navbar />
 
 <slot />
