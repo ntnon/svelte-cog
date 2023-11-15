9186de081bfab9a3d3402ff03540b9839a2168ca
ntnon <Anton.nydal@hotmail.com>
2023-11-15 17:20:43 +0100

    Update wordStore.ts

--- src/stores/wordStore.ts
+++ src/stores/wordStore.ts
@@ -12,15 +12,14 @@ const lang = "norsk"
 const sampleSize = 3;
 
 if (words.length === 0) {
-
     ssm.setItem("words", getRandomSubset(dictionaries[lang], sampleSize));
 }
 
 export const wordStore = writable(words);
 
 function getRandomSubset(wordList: string[], size: number): string[] {
-    const sample = [];
     const totalWords = wordList.length;
+    const sample: string[] = [];
     for (let i = 0; i < size; i++) {
         const randomIndex = Math.floor(Math.random() * totalWords);
         sample.push(wordList[randomIndex]);
@@ -33,37 +32,3 @@ export function resetWords() {
     wordStore.set(newWords)
     ssm.setItem("words", newWords)
 }
-
-/* 
-
-import { settingsStore } from './settingsStore';
-
-function updateWords(lang: string, sampleSize: number) {
-    try {
-        const newWords = getRandomSubset(dictionaries[lang], sampleSize);
-        ssm.setState("words", newWords);
-        return newWords;
-    }
-    catch (err) {
-        console.error(err);
-        return;
-    }
-}
-
-export async function resetWords() {
-    ssm.clearState("words");
-    getWords();
-}
-
-export async function getWords() {
-    console.log("getWords")
-    if (ssm.getState("words") === null) {
-        settingsStore.subscribe(value => {
-            const lang = value.lang;
-            const sampleSize = value.wordRecallSampleSize;
-            updateWords(lang, sampleSize);
-        })
-    }
-    return ssm.getState("words");
-}
-*/
