8c6ab8f8cb6872187cc694465788643df114ac1f
ntnon <Anton.nydal@hotmail.com>
2023-11-15 18:36:06 +0100

    exposing the tracker to all tasks

--- src/routes/tasks/+layout.svelte
+++ src/routes/tasks/+layout.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
 	import Navbar from '../../components/Navbar.svelte';
-	import { updateProgress, getProgress } from '../../stores/progressTracker';
 
+	import { updateProgress, getProgress } from '../../stores/progressTracker';
+	import Routes from '$lib/routes.json';
 	import type { IRoute, IStatus } from '$lib/interfaces';
 	import { setContext } from 'svelte';
 
-	const sharedUpdateProgress = (route: IRoute, status: IStatus['status']) => {
-		updateProgress(route, status);
+	import { page } from '$app/stores';
+
+	const currentPage: IRoute | undefined = Routes.find((r: IRoute) => r.path === $page.route.id);
+	if (!currentPage) throw new Error('current page not found');
+
+	const sharedUpdateProgress = () => {
+		updateProgress(currentPage, 'complete');
 	};
-	const sharedGetProgress = (route: IRoute) => {
-		return getProgress(route);
+	const sharedGetProgress = () => {
+		getProgress(currentPage);
 	};
-	setContext('getProgress', sharedUpdateProgress);
-	setContext('updateProgress', sharedGetProgress);
+	setContext('updateProgress', sharedUpdateProgress);
+	setContext('getProgress', sharedGetProgress);
 </script>
 
 <Navbar />
