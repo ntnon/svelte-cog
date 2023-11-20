13ebb925e46b004278a5aecd4018a380ad6c3ff8
ntnon <Anton.nydal@hotmail.com>
2023-11-20 12:27:03 +0100

    temp css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<div>
+	<slot />
+</div>
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		font-family:
+			system-ui,
+			-apple-system,
+			BlinkMacSystemFont,
+			'Segoe UI',
+			Roboto,
+			Ubuntu,
+			'Helvetica Neue',
+			sans-serif;
+	}
+
+	:global(div) {
+		color: #1b1b1b;
+		background-color: aquamarine;
+		text-decoration: none;
+	}
+</style>
