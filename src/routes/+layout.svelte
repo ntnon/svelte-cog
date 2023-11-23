221fcadc7835115f169d209d0721020a9ef92145
ntnon <Anton.nydal@hotmail.com>
2023-11-23 19:05:06 +0100

    Update +layout.svelte
    removed temporary css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,15 @@
-<div>
+<div class="centered">
 	<slot />
 </div>
 
-<style>
-	:global(body) {
-		margin: 0;
-		padding: 0;
-		font-family:
-			system-ui,
-			-apple-system,
-			BlinkMacSystemFont,
-			'Segoe UI',
-			Roboto,
-			Ubuntu,
-			'Helvetica Neue',
-			sans-serif;
-	}
+<!-- 
+ -->
 
-	:global(div) {
-		color: #1b1b1b;
-		background-color: aquamarine;
-		text-decoration: none;
+<style>
+	.centered {
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+		align-items: center;
 	}
 </style>
