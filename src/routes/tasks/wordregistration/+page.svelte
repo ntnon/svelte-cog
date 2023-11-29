fade30b19d8c37b50c347feb29fa7fd0619591a7
ntnon <Anton.nydal@hotmail.com>
2023-11-29 20:27:47 +0100

    updated to work with wordChecker

--- src/routes/tasks/wordregistration/+page.svelte
+++ src/routes/tasks/wordregistration/+page.svelte
@@ -5,16 +5,33 @@
 	import { onMount } from 'svelte';
 
 	let f: string[] = [];
+	let guess = false;
+	let guesses: string[] = [];
 
+	function checkWords() {
+		for (let i = 0; i < f.length; i++) {
+			if (f[i].toLowerCase() !== guesses[i].toLowerCase()) {
+				console.log('false');
+				return false;
+			}
+		}
+		console.log('true');
+		return true;
+	}
 	onMount(() => {
 		//ensures that the wordStore is populated before the component is mounted
 		f = $wordStore;
-		f = $wordStore ?? [];
 	});
 </script>
 
 <h1>Word Registration</h1>
-{#each f as w}
-	<p>{w}</p>
+{#each f as w, index}
+	{#if guess}
+		<p><input type="text" bind:value={guesses[index]} /></p>
+	{/if}
+	{#if !guess}
+		<p>{w}</p>
+	{/if}
 {/each}
-<button on:click={() => updateProgress($page.route.id, 'complete')}>update progress</button>
+<button on:click={() => (guess = true)}>gjett</button>
+<button on:click={() => checkWords()}>sjekk</button>
