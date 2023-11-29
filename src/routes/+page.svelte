1d9644bfeda3e49b6d519a4a64d20d747a6e7b30
ntnon <Anton.nydal@hotmail.com>
2023-11-29 20:27:10 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,5 @@
 <h1>Welcome to Svelte-Cog!</h1>
 <p>This is a web based cognitive test created with SvelteKit</p>
-<button><a href="/tasks/wordregistration">wordreg</a></button>
+<p>There are 5 tasks</p>
+<p>And a questionaire</p>
+<button><a href="/tasks/wordregistration">Begin</a></button>
