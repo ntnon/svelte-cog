54a5af995e359fa2941c9e94d4ffdc4c80328128
ntnon <Anton.nydal@hotmail.com>
2023-11-29 20:26:38 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+	//logic regarding session storage
+	//if session storage is empty, redirect to home
+	//redirect to consent if no consent is found
+</script>
+
 <div class="centered">
 	<slot />
 </div>
