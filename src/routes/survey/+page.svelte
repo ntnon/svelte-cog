cdf15df89387413647e1b26d585c8fd44a4d857a
ntnon <Anton.nydal@hotmail.com>
2023-12-07 18:02:03 +0100

    reduced survey page to two sub surveys

--- src/routes/survey/+page.svelte
+++ src/routes/survey/+page.svelte
@@ -1,24 +1,4 @@
 <script lang="ts">
-	import SUS from '../../components/feedback/SUS.svelte';
 </script>
 
-<h2>Survey!</h2>
-
-<form>
-	<label for="feedback">Hva synes du om oppgaven "Clock Draw?"</label>
-	<textarea id="feedback" name="feedback" rows="4" cols="50"></textarea>
-	<label for="feedback">Hva synes du om oppgaven "?</label>
-	<textarea id="feedback" name="feedback" rows="4" cols="50"></textarea>
-	<label for="feedback">Hva synes du om oppgaven?</label>
-	<textarea id="feedback" name="feedback" rows="4" cols="50"></textarea>
-	<input type="submit" value="Send inn" />
-</form>
-
-<SUS></SUS>
-
-<style>
-	form {
-		display: flex;
-		flex-direction: column;
-	}
-</style>
+Thanks for completing the tasks! Continue the survey here
