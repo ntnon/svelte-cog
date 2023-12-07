036577e2c58a44d65ef05111a0d97de1bf37f75c
ntnon <Anton.nydal@hotmail.com>
2023-12-07 18:03:40 +0100

    updated to work with new RadioPanel component

---
+++ src/routes/survey/sus/+page.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+	import RadioPanel from '../../../components/survey/RadioPanel.svelte';
+	let content = ['1', '2', '3', '4', '5'];
+
+	const handleSelect = (radioId: string, value: string) => {
+		console.log(radioId, value);
+	};
+
+	const handleSubmit = () => {
+		console.log('f');
+	};
+</script>
+
+<form>
+	<span> 1: Strongly disagree 5: Strongly agree </span>
+	<ol>
+		<li>
+			<label for="frequency">I think that I would like to use this system frequently</label>
+			<RadioPanel
+				on:select={(e) => handleSelect('frequency', e.detail)}
+				{content}
+				radioId={'frequency'}
+			/>
+		</li>
+
+		<li>
+			<label for="complex">I found the system unnecessarily complex</label>
+			<RadioPanel
+				on:select={(e) => handleSelect('complex', e.detail)}
+				{content}
+				radioId={'complex'}
+			/>
+		</li>
+
+		<li>
+			<label for="easy">I thought the system was easy to use</label>
+			<RadioPanel on:select={(e) => handleSelect('easy', e.detail)} {content} radioId={'easy'} />
+		</li>
+
+		<li>
+			<label for="support"
+				>I think that I would need the support of a technical person to be able to use this system</label
+			>
+			<RadioPanel
+				on:select={(e) => handleSelect('support', e.detail)}
+				{content}
+				radioId={'support'}
+			/>
+		</li>
+
+		<li>
+			<label for="integrated"
+				>I found the various functions in this system were well integrated</label
+			>
+			<RadioPanel
+				on:select={(e) => handleSelect('integrated', e.detail)}
+				{content}
+				radioId={'integrated'}
+			/>
+		</li>
+	</ol>
+</form>
+
+<style>
+	form {
+		display: flex;
+		flex-direction: column;
+		max-width: 80%;
+	}
+	span {
+		margin-top: 3rem;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	label {
+		padding-bottom: 1rem;
+	}
+
+	li {
+		padding-bottom: 2rem;
+		padding: 1rem;
+	}
+	.SUSinput {
+		display: flex;
+		flex-direction: row;
+		padding-bottom: 1rem;
+		padding-top: 1rem;
+		width: 100%;
+		justify-content: space-evenly;
+	}
+</style>
---
+++ src/routes/survey/user/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import RadioPanel from '../../../components/survey/RadioPanel.svelte';
+
+	let genders = ['male', 'female', 'other'];
+	let expertise = ['student', 'employed', 'unemployed', 'other'];
+
+	const handleSelect = (radioId: string, value: string) => {
+		console.log(radioId, value);
+	};
+
+	const handleSubmit = () => {
+		console.log('f');
+	};
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+	<ul>
+		<li>
+			gender
+			<RadioPanel
+				on:select={(e) => handleSelect('gender', e.detail)}
+				content={genders}
+				radioId={'gender'}
+				showInside={false}
+			/>
+		</li>
+
+		<li>
+			occupation
+			<RadioPanel
+				on:select={(e) => handleSelect('expertise', e.detail)}
+				content={expertise}
+				radioId={'expertise'}
+				showInside={false}
+			/>
+		</li>
+	</ul>
+	<button type="submit">Submit</button>
+</form>
+
+<style>
+</style>
