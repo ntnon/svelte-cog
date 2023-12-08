2c3309388f7dc15009a862b3248d176eb8a128f7
ntnon <Anton.nydal@hotmail.com>
2023-12-08 15:26:17 +0100

    state storage inside "nydon-website" key

--- src/stores/localStateManager.ts
+++ src/stores/localStateManager.ts
@@ -1,28 +1,36 @@
 import { browser } from "$app/environment"
+const LOCAL_STORAGE_KEY = "nydon-website";
 
 /*
 All saving to local storage management should be done through this class
 */
+
 export const localStateManager = {
     getItem(key: string) {
         if (browser) {
-            return window.localStorage.getItem(key);
+            const storageObject = JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY) || '{}');
+            return storageObject[key];
         }
-        return null
+        return null;
     },
     setItem(key: string, value: unknown) {
         if (browser) {
-            window.localStorage.setItem(key, JSON.stringify(value));
+            const storageObject = JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY) || '{}');
+            console.log("local storage", LOCAL_STORAGE_KEY, key, value)
+            storageObject[key] = value;
+            window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(storageObject));
         }
     },
     clearItem(key: string) {
         if (browser) {
-            window.localStorage.removeItem(key);
+            const storageObject = JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY) || '{}');
+            delete storageObject[key];
+            window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(storageObject));
         }
     },
     clearAll() {
         if (browser) {
-            window.localStorage.clear();
+            window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify({}));
         }
     },
     view() {
--- src/stores/sessionStateManager.ts
+++ src/stores/sessionStateManager.ts
@@ -3,26 +3,33 @@ import { browser } from "$app/environment"
 /*
 All saving to session storage management should be done through this class
 */
+const SESSION_STORAGE_KEY = "nydon-website";
+
 export const sessionStateManager = {
     getItem(key: string) {
         if (browser) {
-            return window.sessionStorage.getItem(key);
+            const storageObject = JSON.parse(window.sessionStorage.getItem(SESSION_STORAGE_KEY) || '{}');
+            return storageObject[key];
         }
-        return null
+        return null;
     },
     setItem(key: string, value: unknown) {
         if (browser) {
-            window.sessionStorage.setItem(key, JSON.stringify(value));
+            const storageObject = JSON.parse(window.sessionStorage.getItem(SESSION_STORAGE_KEY) || '{}');
+            storageObject[key] = value;
+            window.sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify(storageObject));
         }
     },
     clearItem(key: string) {
         if (browser) {
-            window.sessionStorage.removeItem(key);
+            const storageObject = JSON.parse(window.sessionStorage.getItem(SESSION_STORAGE_KEY) || '{}');
+            delete storageObject[key];
+            window.sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify(storageObject));
         }
     },
     clearAll() {
         if (browser) {
-            window.sessionStorage.clear();
+            window.sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify({}));
         }
     },
     viewSessionState() {
@@ -30,11 +37,11 @@ export const sessionStateManager = {
             console.log("session storage: ", window.sessionStorage)
         }
     },
-    getItemArray(key: string) { //if a key contains an array, this will return it
-        const storedWords = this.getItem(key);
-        if (storedWords && storedWords !== "undefined") {
-            const array = JSON.parse(storedWords) as [];
-            return array.length !== 0 ? array : undefined;
+    getItemArray(key: string) {
+        if (browser) {
+            const storageObject = JSON.parse(window.sessionStorage.getItem(SESSION_STORAGE_KEY) || '{}');
+            return storageObject[key] ? storageObject[key] : undefined;
         }
+        return undefined;
     },
 };
