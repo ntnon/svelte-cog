7eb6402c37d299b16fa1a9ed0104b85ff510cd7d
ntnon <Anton.nydal@hotmail.com>
2023-12-08 15:27:51 +0100

    moved consentlogic to consentform component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,14 @@
 <script lang="ts">
 	import ConsentForm from '../components/consent/ConsentForm.svelte';
 	import Nav from '../components/navigation/Nav.svelte';
-	import { localStateManager as lsm } from '../stores/localStateManager';
-	import { onMount } from 'svelte';
-	import NextNav from '../components/navigation/NextNav.svelte';
 
-	let storedConsent: boolean;
-	onMount(() => {
-		storedConsent = lsm.getItem('consent') === 'true' ? true : false;
-	});
+	import NextNav from '../components/navigation/NextNav.svelte';
 </script>
 
 <NextNav />
 
 <div class="centered">
-	{#if !storedConsent}
-		<ConsentForm bind:storedConsent />
-	{/if}
+	<ConsentForm />
 	<Nav />
 	<slot />
 </div>
