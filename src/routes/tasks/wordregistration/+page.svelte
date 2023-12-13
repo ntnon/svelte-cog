480e362121999917f515886b8f6862b213a512ef
ntnon <Anton.nydal@hotmail.com>
2023-12-13 16:00:44 +0100

    added 'completed check'

--- src/routes/tasks/wordregistration/+page.svelte
+++ src/routes/tasks/wordregistration/+page.svelte
@@ -9,15 +9,17 @@
 	let words: string[] = [];
 	let guess = false;
 	let guesses: string[] = [];
+	let complete = false;
 
 	function checkWords() {
 		for (let i = 0; i < words.length; i++) {
+			if (guesses.length !== words.length) {
+				return false;
+			}
 			if (words[i].toLowerCase() !== guesses[i].toLowerCase()) {
-				console.log('false');
 				return false;
 			}
 		}
-		console.log('true');
 		return true;
 	}
 
@@ -32,24 +34,55 @@
 	}
 	onMount(() => {
 		words = ssm.getItemArray('words');
-		if (words.length === 0 || words.length !== wordCount) {
+		if (words === undefined || words.length === 0 || words.length !== wordCount) {
+			// if the first condition is met the subsequent are not checked
 			let newWords = randomWords(dictionaries[language], wordCount);
 			ssm.setItem('words', newWords);
 			words = newWords;
 		}
-
-		//ensures that the wordStore is populated before the component is mounted
 	});
+
+	function handleInput(e: Event, index: number) {
+		guesses[index] = (e.target as HTMLInputElement).value;
+		console.log(guesses[index], words[index]);
+		if (checkWords()) {
+			complete = true;
+		}
+	}
 </script>
 
 <h1>Word Registration</h1>
+<button on:click={() => (guess ? (guess = false) : (guess = true))}
+	>{guess ? 'Vis ord' : 'Gjett'}</button
+>
+{#if complete}
+	GRATTIS!
+{/if}
 {#each words as w, index}
 	{#if guess}
-		<p><input type="text" bind:value={guesses[index]} /></p>
+		<p>
+			<input
+				class={guesses[index] === w.toLowerCase() ? 'correct' : 'incorrect'}
+				type="text"
+				on:input={(e) => handleInput(e, index)}
+			/>
+		</p>
 	{/if}
 	{#if !guess}
 		<p>{w}</p>
 	{/if}
 {/each}
-<button on:click={() => (guess = true)}>gjett</button>
-<button on:click={() => checkWords()}>sjekk</button>
+
+<style>
+	input[type='text']:focus {
+		outline: 2px solid rgb(0, 0, 0); /* Change as needed */
+	}
+
+	.correct {
+		border: green 3px solid;
+	}
+
+	.incorrect {
+		border: red 3px solid;
+	}
+</style>
