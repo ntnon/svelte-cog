c45ec52e2cf6f9859d343f4947721edc38501803
ntnon <Anton.nydal@hotmail.com>
2023-12-15 10:18:04 +0100

    Update feedback labels in survey page

--- src/routes/survey/tasks/+page.svelte
+++ src/routes/survey/tasks/+page.svelte
@@ -6,9 +6,9 @@
 <form>
 	<label for="feedback">Hva synes du om oppgaven "Clock Draw?"</label>
 	<textarea id="feedback" name="feedback" rows="4" cols="50"></textarea>
-	<label for="feedback">Hva synes du om oppgaven "?</label>
+	<label for="feedback">Hva synes du om oppgaven "Word Registration"?</label>
 	<textarea id="feedback" name="feedback" rows="4" cols="50"></textarea>
-	<label for="feedback">Hva synes du om oppgaven?</label>
+	<label for="feedback">Hva synes du om oppgaven "Word Recall"?</label>
 	<textarea id="feedback" name="feedback" rows="4" cols="50"></textarea>
 	<input type="submit" value="Send inn" />
 </form>
