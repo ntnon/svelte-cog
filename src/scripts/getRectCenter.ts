e8dc346f2a5e5d5eea355aea464b65959f108749
ntnon <Anton.nydal@hotmail.com>
2023-12-31 00:51:06 +0100

    renamed function

---
+++ src/scripts/getRectCenter.ts
@@ -0,0 +1,11 @@
+import type { IPosition } from '../lib/interfaces';
+
+export const getRectCenter = (e: HTMLElement) => {
+    const rect = e.getBoundingClientRect();
+    const scrollLeft = window.scrollX || document.documentElement.scrollLeft;
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    return {
+        left: rect.left + rect.width / 2 + scrollLeft,
+        top: rect.top + rect.height / 2 + scrollTop
+    } as IPosition;
+}
