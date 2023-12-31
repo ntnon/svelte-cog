96e419ea1576f7877d9e77d9a2396a05aa18635b
ntnon <Anton.nydal@hotmail.com>
2023-12-31 01:24:47 +0100

    updated to work with dispatch in draggable

--- src/routes/tasks/clockdraw/+page.svelte
+++ src/routes/tasks/clockdraw/+page.svelte
@@ -12,40 +12,21 @@
 		blocks.update((b) => b.map((b) => ({ ...b, position: { top: 0, left: 0 }, placed: false })));
 	}
 
-	const onMouseUpFn = (
-		e: MouseEvent | TouchEvent,
-		block: IBlock,
-		position: IPosition | undefined
-	) => {
-		// Update the block store
-		blocks.update((blocks) => {
-			return blocks.map((b) => {
-				if (b.id === block.id) {
-					// Update the position of the block
-					return { ...b, position };
-				} else {
-					return b;
-				}
-			});
-		});
-	};
-
-	const onMouseDownFn = (e: MouseEvent | TouchEvent, block: IBlock) => {
-		//implement logic
-	};
+	function handlePositionChange(block: IBlock, newPos: IPosition) {
+		//update position block's position in blockstore?
+	}
 </script>
 
-<h2>Klokketesten</h2>
-<p>Gjenskap klokken! Dra tallene til riktig plassering på klokken.</p>
+<h2>The Clock Test</h2>
+<p>Drag the numbers to their right place on the clock</p>
 <button on:click={() => restart()}>Restart</button>
 <div class="numberBlocks">
 	<div class="hour-block-list">
 		{#each $blocks as block (block.id)}
 			<Draggable
-				item={block}
 				position={block.position}
-				onMouseDownFn={(e) => onMouseDownFn(e, block)}
-				onMouseUpFn={(e, position) => onMouseUpFn(e, block, position)}>{block.id}</Draggable
+				on:positionChange={(e) => handlePositionChange(block, e.detail.newPosition)}
+				>{block.id}</Draggable
 			>
 		{/each}
 	</div>
@@ -64,5 +45,6 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+		max-width: 50%;
 	}
 </style>
