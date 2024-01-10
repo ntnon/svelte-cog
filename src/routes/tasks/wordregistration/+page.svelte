0c223c3e1e9342c63c1fe1204763b7ddc0218fa2
ntnon <Anton.nydal@hotmail.com>
2024-01-10 14:56:32 +0100

    moved word generation to sessionstatemanager

--- src/routes/tasks/wordregistration/+page.svelte
+++ src/routes/tasks/wordregistration/+page.svelte
@@ -1,51 +1,42 @@
 <script lang="ts">
-	import settings from '../../../lib/settings.json';
-	import dictionaries from '../../../lib/words.json';
 	import { onMount } from 'svelte';
 	import { sessionStateManager as ssm } from '../../../stores/sessionStateManager';
 	import RecallComponent from '../../../components/tasks/RecallComponent.svelte';
+	import type { ITaskData } from '$lib/dataInterfaces';
+	import type { IRoute } from '$lib/interfaces';
+	import { guessStore } from '../../../stores/guessStore';
 
-	let language = settings.language as keyof typeof dictionaries;
-	let wordCount = settings.wordRecallCount;
-	let words: string[] = [];
-	let recallMode = false;
-	let complete = false;
+	let words: string[];
 
-	//generates a random set of words from a list of words
-	function randomWords(wordList: string[], size: number): string[] {
-		const totalWords = wordList.length;
-		const sample: string[] = [];
-		for (let i = 0; i < size; i++) {
-			const randomIndex = Math.floor(Math.random() * totalWords);
-			sample.push(wordList[randomIndex]);
-		}
-		return sample;
-	}
-
-	//ensures that some words populate the "guessable words"
 	onMount(() => {
-		words = ssm.getItemArray('words');
-		if (words === undefined || words.length === 0 || words.length !== wordCount) {
-			// if the first condition is met the subsequent are not checked
-			let newWords = randomWords(dictionaries[language], wordCount);
-			ssm.setItem('words', newWords);
-			words = newWords;
-		}
+		words = ssm.getWords();
+		guessStore.clear();
 	});
+	let recallMode = false;
+
+	let route: IRoute = {
+		name: 'Word Registration',
+		path: '/tasks/wordregistration'
+	};
+
+	let taskData: ITaskData = {
+		route: route,
+		complete: false,
+		score: 0,
+		corrections: 0
+	};
 </script>
 
 <h1>Word Registration</h1>
 <button on:click={() => (recallMode ? (recallMode = false) : (recallMode = true))}
 	>{recallMode ? 'Show words' : 'Guess'}</button
 >
-{#if complete}
-	GRATTIS!
-{/if}
 {#if recallMode}
-	<RecallComponent {complete} />
+	<RecallComponent {taskData} {words} />
 {/if}
-{#each words as w}
-	{#if !recallMode}
+
+{#if words && !recallMode}
+	{#each words as w}
 		<p>{w}</p>
-	{/if}
-{/each}
+	{/each}
+{/if}
--- src/stores/sessionStateManager.ts
+++ src/stores/sessionStateManager.ts
@@ -1,47 +1,74 @@
 import { browser } from "$app/environment"
+import settings from "../lib/settings.json";
+import dictionary from '../lib/words.json';
 
 /*
 All saving to session storage management should be done through this class
 */
+
+const wordCount = settings.wordRecallCount;
+
+function randomWords(wordList: string[]) {
+    const totalWords = wordList.length;
+    const sample = new Set<string>();
+    while (sample.size < wordCount) {
+        const randomIndex = Math.floor(Math.random() * totalWords);
+        sample.add(dictionary[randomIndex]);
+    }
+    return Array.from(sample);
+}
+
 const SESSION_STORAGE_KEY = "nydon-website";
 
 export const sessionStateManager = {
-    getItem(key: string) {
-        if (browser) {
-            const storageObject = JSON.parse(window.sessionStorage.getItem(SESSION_STORAGE_KEY) || '{}');
-            return storageObject[key];
+    getWords() {
+        if (!browser) return null;
+        const storageObject = JSON.parse(window.sessionStorage.getItem(SESSION_STORAGE_KEY) || '{}');
+        console.log("storage object: ", storageObject)
+        if (storageObject.words.length !== wordCount) {
+            console.log("hi")
+            storageObject.words = randomWords(dictionary);
+            window.sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify(storageObject));
         }
-        return null;
+        return storageObject.words;
+
+    },
+    getItem(key: string) {
+        if (!browser) return null;
+        const storageObject = JSON.parse(window.sessionStorage.getItem(SESSION_STORAGE_KEY) || '{}');
+        return storageObject[key];
     },
     setItem(key: string, value: unknown) {
-        if (browser) {
-            const storageObject = JSON.parse(window.sessionStorage.getItem(SESSION_STORAGE_KEY) || '{}');
-            storageObject[key] = value;
-            window.sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify(storageObject));
-        }
+        if (!browser) return null;
+
+        const storageObject = JSON.parse(window.sessionStorage.getItem(SESSION_STORAGE_KEY) || '{}');
+        storageObject[key] = value;
+        window.sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify(storageObject));
+
     },
     clearItem(key: string) {
-        if (browser) {
-            const storageObject = JSON.parse(window.sessionStorage.getItem(SESSION_STORAGE_KEY) || '{}');
-            delete storageObject[key];
-            window.sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify(storageObject));
-        }
+        if (!browser) return null;
+        const storageObject = JSON.parse(window.sessionStorage.getItem(SESSION_STORAGE_KEY) || '{}');
+        delete storageObject[key];
+        window.sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify(storageObject));
+
     },
     clearAll() {
-        if (browser) {
-            window.sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify({}));
-        }
+        if (!browser) return null;
+
+        window.sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify({}));
+
     },
     viewSessionState() {
-        if (browser) {
-            console.log("session storage: ", window.sessionStorage)
-        }
+        if (!browser) return null;
+
+        console.log("session storage: ", window.sessionStorage)
+
     },
     getItemArray(key: string) {
-        if (browser) {
-            const storageObject = JSON.parse(window.sessionStorage.getItem(SESSION_STORAGE_KEY) || '{}');
-            return storageObject[key] ? storageObject[key] : undefined;
-        }
-        return undefined;
+        if (!browser) return null;
+        const storageObject = JSON.parse(window.sessionStorage.getItem(SESSION_STORAGE_KEY) || '{}');
+        return storageObject[key] ? storageObject[key] : null;
+
     },
 };
