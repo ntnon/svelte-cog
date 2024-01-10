6409d546a5ea59ac55c586e3375cd5e4344ebf76
ntnon <Anton.nydal@hotmail.com>
2024-01-10 14:57:30 +0100

    Update word recall page with task data

--- src/routes/tasks/wordrecall/+page.svelte
+++ src/routes/tasks/wordrecall/+page.svelte
@@ -1,6 +1,28 @@
 <script lang="ts">
+	import type { ITaskData } from '$lib/dataInterfaces';
+	import { onMount } from 'svelte';
 	import RecallComponent from '../../../components/tasks/RecallComponent.svelte';
+	import { sessionStateManager as ssm } from '../../../stores/sessionStateManager';
+	import { guessStore } from '../../../stores/guessStore';
+
+	let words: string[];
+
+	onMount(() => {
+		console.log('mount');
+		words = ssm.getWords();
+		guessStore.clear();
+	});
+
+	let taskData: ITaskData = {
+		route: {
+			name: 'Word Recall',
+			path: '/tasks/wordrecall'
+		},
+		complete: false,
+		score: 0,
+		corrections: 0
+	};
 </script>
 
 <h1>Word recall</h1>
-<RecallComponent />
+<RecallComponent {taskData} />
