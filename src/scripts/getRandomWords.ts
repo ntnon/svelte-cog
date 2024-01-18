4a4ec8a22b3e0fdff2e3f45d91d2ad8a3e21c3b4
ntnon <Anton.nydal@hotmail.com>
2024-01-18 19:13:31 +0100

    Create getRandomWords.ts

---
+++ src/scripts/getRandomWords.ts
@@ -0,0 +1,13 @@
+import settings from "../lib/settings.json";
+import dictionary from '../lib/words.json';
+
+export const getRandomWords = () => {
+    const count = settings.wordRecallCount;
+    const totalWords = dictionary.length;
+    const sample = new Set<string>();
+    while (sample.size < count) {
+        const randomIndex = Math.floor(Math.random() * totalWords);
+        sample.add(dictionary[randomIndex]);
+    }
+    return Array.from(sample);
+}
