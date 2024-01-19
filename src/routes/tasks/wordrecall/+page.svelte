aafd88921b889e035652f158dd4448fb02e0a7d0
ntnon <Anton.nydal@hotmail.com>
2024-01-19 15:09:36 +0100

    Update word registration store name to wordRecall

--- src/routes/tasks/wordrecall/+page.svelte
+++ src/routes/tasks/wordrecall/+page.svelte
@@ -3,7 +3,7 @@
 	import type { ITaskData } from '$lib/dataInterfaces';
 	import { getDataStore } from '$lib/state.svelte';
 
-	const store = getDataStore<ITaskData>('wordregistration');
+	const store = getDataStore<ITaskData>('wordRecall');
 </script>
 
 <h2>Word recall</h2>
