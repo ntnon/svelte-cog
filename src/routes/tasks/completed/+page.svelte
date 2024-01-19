e77907cb280eb16f79ff9d1a675189dcde075068
ntnon <Anton.nydal@hotmail.com>
2024-01-19 15:10:01 +0100

    Add task data display in completed page

--- src/routes/tasks/completed/+page.svelte
+++ src/routes/tasks/completed/+page.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
+	import type { ITaskData } from '$lib/dataInterfaces';
+	import { getDataStore } from '$lib/state.svelte';
+
+	const wordRegistration = getDataStore<ITaskData>('wordRegistration');
+	const wordRecall = getDataStore<ITaskData>('wordRecall');
+	const clockDraw = getDataStore<ITaskData>('clockDraw');
+	const clockPoint = getDataStore<ITaskData>('clockPoint');
+
+	const allStores = [$wordRecall, $wordRegistration, $clockDraw, $clockPoint];
+	console.log($wordRegistration);
 </script>
 
 <h1>Tasks completed!</h1>
 <p>Here are your answers:</p>
 <p>Well done!</p>
+
+{#each allStores as store}
+	<p>Task: {store.id}</p>
+	<p>Score: {store.score}</p>
+	<p>Corrections: {store.corrections}</p>
+{/each}
