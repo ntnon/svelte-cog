f94a384d9ea4824f7951e1cc7627fe5d7744140b
ntnon <Anton.nydal@hotmail.com>
2024-01-22 16:35:14 +0100

    changed from svelte-cog to web-cog

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 </script>
 
-<h1>Welcome to Svelte-Cog!</h1>
+<h1>Welcome to Web-Cog!</h1>
 <p>This is a web based cognitive test created with SvelteKit</p>
 <p>There are 5 tasks</p>
 <p>And a questionaire</p>
