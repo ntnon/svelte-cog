aae0438498d2ff523df7fac438ff49467b0f622a
ntnon <Anton.nydal@hotmail.com>
2024-01-23 14:18:44 +0100

    Update +page.svelte

--- src/routes/tasks/clockdraw/+page.svelte
+++ src/routes/tasks/clockdraw/+page.svelte
@@ -1,114 +1,163 @@
 <script lang="ts">
-	import type { IBlock, IPosition } from '$lib/interfaces';
-	import type { ITaskData } from '$lib/dataInterfaces';
-	import Draggable from '../../../components/Draggable.svelte';
+	import type { ITaskMarkers } from '$lib/dataInterfaces';
 	import Clock from '../../../components/Clock.svelte';
+	import { getDataStore } from '$lib/state.svelte';
+	import type { InteractionEvent } from '$lib/types';
+	import { getClientCoordinates } from '../../../scripts/getClientCoordinates';
+	import { calculateMouseDialAngle } from '../../../scripts/calculateMouseDialAngle';
+	import { cssRotationToClockHours } from '../../../scripts/cssRotationToClockHours';
 	import { getPagePosition } from '../../../scripts/getPagePosition';
 
-	import type { SvelteComponent } from 'svelte';
+	const store = getDataStore<ITaskMarkers>('clockDraw');
 
-	import { getDataStore } from '$lib/state.svelte';
-
-	const store = getDataStore<ITaskData>('clockdraw');
-
-	let blocksIDsInsideClock = new Set<number>();
-	let placedBlockIDs = new Set<number>(); // this is used to check if the block has been placed before
+	let dial: HTMLElement;
 
 	const calculateScore = () => {
-		//calculate score
-		//if score is 12, set complete to true
-		return undefined;
+		let newScore = 0;
+		$store.markers.map((m) => {
+			newScore += 1;
+		});
+
+		return newScore;
 	};
 
-	const calculateComplete = () => {
-		if (blocksIDsInsideClock.size === blockCount) {
-			$store.complete = true;
+	const isMarkerInCircle = (markerHTMLElement: HTMLElement) => {
+		const clockHTMLElement = document.getElementById('clock');
+		if (!clockHTMLElement) {
+			console.warn('clock not found');
+			return false;
 		}
-	};
-
-	// "Block" is used to describe a draggable number
-	const blockCount = 12; // should be 12
-	const newBlocks = (num: number) => {
-		return Array(num)
-			.fill(null)
-			.map((_, i) => ({
-				id: i,
-				name: i + 1,
-				position: { top: 0, left: 0 }
-			}));
-	};
-	let blocks: IBlock[] = newBlocks(blockCount);
-
-	// clock variables
-	let clock: SvelteComponent;
-	let clockHTMLElement: HTMLElement;
-
-	// event handlers
-	const handleMouseUp = (block: IBlock, draggableElement: HTMLElement) => {
-		placedBlockIDs = new Set([...placedBlockIDs, block.id]);
-		if (isBlockInCircle(draggableElement)) {
-			blocksIDsInsideClock = new Set([...blocksIDsInsideClock, block.id]);
+		if (!markerHTMLElement) {
+			console.warn('marker not found');
+			return false;
 		}
-		calculateComplete();
-	};
-
-	const handlePositionChange = (block: IBlock, newPos: IPosition) => {
-		blocks[block.id].position = newPos;
-		calculateComplete();
-		calculateScore();
-	};
-
-	const handleMouseDown = (block: IBlock, draggableElement: HTMLElement) => {
-		if (placedBlockIDs.has(block.id)) {
-			$store.corrections += 1;
-			console.log('corrections', $store.corrections);
-		}
-	};
-
-	// logic
-	const isBlockInCircle = (draggableElement: HTMLElement) => {
-		const blockPos = getPagePosition(draggableElement);
-		const clockPos = getPagePosition(clockHTMLElement);
+		const markerPosition = getPagePosition(markerHTMLElement);
+		const clockPosition = getPagePosition(clockHTMLElement);
 		const clockRadius = clockHTMLElement.offsetWidth / 2;
-		const dx = clockPos.x - blockPos.x;
-		const dy = clockPos.y - blockPos.y;
+		const dx = clockPosition.x - markerPosition.x;
+		const dy = clockPosition.y - markerPosition.y;
 		const distance = Math.sqrt(dx * dx + dy * dy);
 		const overlap = distance <= clockRadius;
+		console.log(overlap);
 		return overlap;
+	};
+
+	const handleMouseMove = (e: InteractionEvent, touch: boolean = false) => {
+		if (!touch) {
+			e.preventDefault();
+		}
+		const currentMarker = $store.markers.find((m) => m.active === true);
+		if (!currentMarker) {
+			return;
+		}
+		const currentMarkerHTMLElement = document.getElementById('marker-' + currentMarker.id);
+
+		if (!currentMarkerHTMLElement) {
+			return;
+		}
+		const { x: offsetX, y: offsetY } = getPagePosition(currentMarkerHTMLElement);
+		const { clientX, clientY } = getClientCoordinates(e);
+		const newAngle = calculateMouseDialAngle(dial, clientX, clientY);
+		const newPointsAt = cssRotationToClockHours(newAngle);
+		const newInsideClock = isMarkerInCircle(currentMarkerHTMLElement);
+		const newMarkers = $store.markers.map((m) => {
+			if (m.id === currentMarker?.id) {
+				return {
+					...currentMarker,
+					pointsAt: newPointsAt,
+					angle: newAngle,
+					x: clientX - offsetX,
+					y: clientY - offsetY,
+					isInsideClock: newInsideClock
+				};
+			}
+			return m;
+		});
+		store.update((value) => ({ ...value, markers: newMarkers }));
+		$store.score = calculateScore();
 	};
 </script>
 
-<h2>The Clock Test</h2>
-<p>Drag the numbers to their right place on the clock</p>
-<div class="numberBlocks">
-	<div class="hour-block-list">
-		{#each blocks as block (block.id)}
-			<Draggable
-				position={block.position}
-				on:positionChange={(e) => handlePositionChange(block, e.detail.position)}
-				on:mouseUp={(e) => handleMouseUp(block, e.detail.draggableElement)}
-				on:mouseDown={(e) => handleMouseDown(block, e.detail.draggableElement)}
-				>{block.name}</Draggable
+{$store.score}
+<button
+	on:click={() =>
+		($store.markers = $store.markers.map((m) => ({
+			...m,
+			active: false,
+			x: 0,
+			y: 0,
+			pointsAt: undefined,
+			isInsideClock: false,
+			angle: undefined
+		})))}>Reset</button
+>
+<span class="markerlist" id="markers">
+	{#each $store.markers as marker}
+		<div id={'marker-' + marker.id}>
+			<div
+				id={marker.id.toString()}
+				role="button"
+				tabindex="0"
+				class="marker"
+				style="top: {marker.y}px;
+			left: {marker.x}px;"
+				on:mousedown={() => (marker.active = true)}
+				on:mouseup={() => (marker.active = false)}
+				on:touchstart={() => (marker.active = true)}
+				on:touchend={() => (marker.active = false)}
 			>
-		{/each}
-	</div>
-	<div bind:this={clockHTMLElement}>
-		<Clock bind:this={clock} />
-	</div>
-</div>
+				{marker.id}
+			</div>
+		</div>
+	{/each}
+</span>
+<Clock>
+	<div class="dial" bind:this={dial}></div>
+</Clock>
+
+<svelte:window
+	on:mouseup={() => $store.markers.map((m) => (m.active = false))}
+	on:touchend={() => $store.markers.map((m) => (m.active = false))}
+	on:mousemove={handleMouseMove}
+	on:touchmove={(e) => handleMouseMove(e, true)}
+/>
 
 <style>
-	.hour-block-list {
+	.marker {
+		touch-action: none;
+		user-select: none;
+		cursor: pointer;
+		border: solid 3px #3c6ca8;
+		position: relative;
+		z-index: 1;
+		width: 3rem;
+		height: 3rem;
+		min-height: 3rem;
+		min-width: 3rem;
+		border-radius: 50%;
+		text-align: center;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		box-sizing: border-box;
+		margin: 0.1rem;
+		background-color: #f4ebec;
+	}
+	.dial {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		width: 10px;
+		height: 10px;
+		background: rgb(128, 127, 127);
+		border-radius: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	.markerlist {
 		display: flex;
 		flex-direction: row;
 		flex-wrap: wrap;
-		justify-content: space-between;
-	}
-
-	.numberBlocks {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		max-width: 50%;
+		width: 80%;
 	}
 </style>
