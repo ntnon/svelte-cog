f6a6ecc862bb1af7ea6df233363ab17d885ab6e5
ntnon <Anton.nydal@hotmail.com>
2024-01-23 16:53:09 +0100

    Refactor RecallComponent.svelte to improve score calculation and completion check

--- src/components/RecallComponent.svelte
+++ src/components/RecallComponent.svelte
@@ -6,27 +6,29 @@
 
 	export let store: Writable<ITaskGuesses>;
 
-	let guesses: string[] = $store.guesses ?? [];
 	let wordStore = getDataStore<string[]>('words');
 
 	const calculateScore = () => {
-		let correctGuesses = Array.from(guesses as string[]).filter((v) => $wordStore.includes(v));
-		$store.score = correctGuesses.length;
-		if ($store.score === $wordStore.length) {
-			$store.success = true;
+		let correctGuesses = Array.from(new Set($store.guesses as string[])).filter((v) =>
+			$wordStore.includes(v)
+		);
+		return correctGuesses.length;
+	};
+
+	const calculateComplete = () => {
+		if ($store.guesses.length === $wordStore.length && !$store.guesses.includes('')) {
+			return true;
 		}
-		if (guesses.length === $wordStore.length) {
-			$store.complete = true;
-		}
+		return false;
 	};
 
 	//triggered whenever a user make changes
 	const handleInput = (e: Event, index: number) => {
 		const input = (e.target as HTMLInputElement).value;
 		let validatedInput = validateInput(input) ? input.toLowerCase() : '';
-		guesses[index] = validatedInput;
-		$store.guesses = guesses;
-		calculateScore();
+		$store.guesses[index] = validatedInput;
+		$store.score = calculateScore();
+		$store.complete = calculateComplete();
 	};
 
 	const handleBlur = (e: Event) => {
@@ -38,12 +40,16 @@
 {#each $wordStore as w, index}
 	<p>
 		<input
-			class={guesses[index] === w ? 'correct' : 'incorrect'}
+			class={$wordStore.includes($store.guesses[index]) &&
+			new Set($store.guesses).size === $store.guesses.length
+				? 'correct'
+				: 'incorrect'}
 			type="text"
-			value={guesses[index] ?? ''}
+			value={$store.guesses[index] ?? ''}
 			on:input={(e) => handleInput(e, index)}
 			on:blur={(e) => handleBlur(e)}
 		/>
+		{new Set($store.guesses).size}
 	</p>
 {/each}
 
