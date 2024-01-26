027a61967c932902a5e3e64403eb6b7d047a2e3a
ntnon <Anton.nydal@hotmail.com>
2024-01-26 18:12:21 +0100

    consolidate context into one

--- src/lib/dataInterfaces.ts
+++ src/lib/dataInterfaces.ts
@@ -1,34 +1,25 @@
-interface IResult {
-    id: string; // must be unique - check existing entries ? or create SHA256 based on timestamp
-    settings: IMetaData;
-    date: Date;
-    taskData: ITaskData[];
-    error: string[];
+import type { Writable } from "svelte/store";
+
+interface IPage {
+    path: string,
+    enableNext: boolean,
+    name: string,
+    timesVisited: number,
 }
 
-interface ITaskData {
-    id: string;
-    name: string;
-    complete: boolean;
+interface ITaskPage extends IPage {
     corrections: number;
     score: number;
-    comment: string;
     success: boolean;
-}
-
-interface ITaskGuesses extends ITaskData {
-    guesses: string[];
-    showWords: boolean;
-}
-
-interface ITaskHands extends ITaskData {
-    minute: IHand;
-    hour: IHand;
-    targetTimestamp: ITimeStamp;
-}
-
-interface ITaskMarkers extends ITaskData {
-    markers: IMarker[];
+    comment?: string;
+    guesses?: string[];
+    showWords?: boolean;
+    hands?: {
+        targetTimestamp: ITimeStamp,
+        hour: IHand,
+        minute: IHand,
+    };
+    markers?: IMarker[];
 }
 
 interface ITimeStamp {
@@ -38,7 +29,7 @@ interface ITimeStamp {
 }
 
 interface IHand {
-    name: 'hour' | 'minute';
+    name: string;
     angle: number;
     active: boolean;
     pointsAt?: number;
@@ -55,32 +46,56 @@ interface IMarker {
     isInsideClock?: boolean,
 }
 
-interface ISUS {
-    id: string;
-    question: string;
-    score?: number;
-}
 
-interface IRoute {
-    name: string;
-    path: string;
-    complete: boolean;
-}
+interface ISUSPage extends IPage {
+    questions: [{
+        id: string;
+        question: string;
+        score?: number;
+    }
+    ];
 
+}
 
 interface IMetaData {
     date: Date;
     version: string;
-    wordRegistrationCount: number;
-    language: "english" | "norsk";
 }
 
-interface IUserData {
-    id: string;
-    phone: number;
-    gender?: "male" | "female" | "other" | "prefer not to say";
-    age?: number;
-    domain?: "design" | "UX" | "web development" | "programming" | "caregiver" | "games" | "psychology" | "other" | string; //come up with more examples
+interface IUser extends IPage {
+    phone?: number | undefined;
+    gender?: "male" | "female" | "other" | "prefer not to say" | undefined;
+    age?: number | undefined;
+    domain?: "design" | "UX" | "web development" | "programming" | "caregiver" | "games" | "psychology" | "other" | string | undefined; //come up with more examples
 }
 
-export type { IResult, ITaskData, IMetaData, IUserData, ISUS, IRoute, IHand, ITaskGuesses, ITaskHands, ITaskMarkers, IMarker }
+
+interface ISettings {
+    wordcount: number;
+    language: string;
+    storageKey: string;
+}
+
+interface IAppData {
+    metadata: IMetaData,
+    settings: ISettings
+    pages: {
+        home: Writable<IPage>,
+        wordregistration: Writable<ITaskPage>,
+        clockpoint: Writable<ITaskPage>,
+        clockdraw: Writable<ITaskPage>,
+        wordrecall: Writable<ITaskPage>,
+        result: Writable<IPage>,
+        survey: Writable<IPage>,
+        user: Writable<IUser>,
+        sus: Writable<ISUSPage>,
+        about: Writable<IPage>,
+        end: Writable<IPage>
+    },
+    data: {
+        words: Writable<string[]>,
+        consent: Writable<boolean>,
+    }
+}
+
+export type { ITaskPage, IPage, IMetaData, IUser, ISUSPage, IHand, IMarker, IAppData, ISettings }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,97 +1,12 @@
 <script lang="ts">
-	import type {
-		ISUS,
-		ITaskData,
-		IRoute,
-		ITaskHands,
-		ITaskMarkers,
-		ITaskGuesses
-	} from '$lib/dataInterfaces';
-	import { getData } from '$lib/dataService';
-	import { getDataStore, setDataStore } from '$lib/state.svelte';
-
+	import { getAppData } from '$lib/dataService';
+	import { setDataStore } from '$lib/state.svelte';
 	import Nav from '../components/navigation/Nav.svelte';
-	import susQuestions from '$lib/susQuestions.json';
-	import { getRandomTimeStamp } from '../scripts/getRandomTimeStamp';
-	import NewNav from '../components/NewNav.svelte';
-	import { get } from 'svelte/store';
-	const { data } = $$props; // load data from server
 
-	const createTaskData = (id: string, name: string) => {
-		return {
-			id: id,
-			name: name,
-			complete: false,
-			score: 0,
-			corrections: 0,
-			comment: '',
-			success: false
-		};
-	};
+	const allData = getAppData();
 
-	const clockPointData = getData<ITaskHands>('clockpoint', 'session', {
-		...createTaskData('clockpoint', 'Clock Point'),
-		minute: { name: 'minute', angle: 25, active: false },
-		hour: { name: 'hour', angle: 90, active: false },
-		targetTimestamp: getRandomTimeStamp()
-	});
-	const clockDrawData = getData<ITaskMarkers>('clockdraw', 'session', {
-		...createTaskData('clockdraw', 'Clock Draw'),
-		markers: [
-			{ id: 1, x: 0, y: 0, active: false },
-			{ id: 2, x: 0, y: 0, active: false },
-			{ id: 3, x: 0, y: 0, active: false },
-			{ id: 4, x: 0, y: 0, active: false },
-			{ id: 5, x: 0, y: 0, active: false },
-			{ id: 6, x: 0, y: 0, active: false },
-			{ id: 7, x: 0, y: 0, active: false },
-			{ id: 8, x: 0, y: 0, active: false },
-			{ id: 9, x: 0, y: 0, active: false },
-			{ id: 10, x: 0, y: 0, active: false },
-			{ id: 11, x: 0, y: 0, active: false },
-			{ id: 12, x: 0, y: 0, active: false }
-		]
-	});
-	const wordRegistrationData = getData<ITaskGuesses>('wordregistration', 'session', {
-		...createTaskData('wordregistration', 'Word Registration'),
-		guesses: [],
-		showWords: false
-	});
-	const wordRecallData = getData<ITaskGuesses>('wordrecall', 'session', {
-		...createTaskData('wordrecall', 'Word Recall'),
-		guesses: [],
-		showWords: false
-	});
-
-	const consentData = getData<boolean>('consent', 'session', false);
-	const wordData = getData<string[]>('words', 'session', data.words); //fetched from server
-	const susData = getData<ISUS[]>('sus', 'session', susQuestions);
-
-	const routerData = getData<IRoute[]>('routes', 'session', [
-		{ path: '/', name: 'Home', complete: false },
-		{ path: '/tasks/wordregistration', name: 'Word Registration', complete: false },
-		{ path: '/tasks/clockdraw', name: 'Clock Draw', complete: false },
-		{ path: '/tasks/clockpoint', name: 'Clock Point', complete: false },
-		{ path: '/tasks/wordrecall', name: 'Word Recall', complete: false },
-		{ path: '/tasks/completed', name: 'Completed', complete: true },
-		{ path: '/survey/user', name: 'User', complete: false },
-		{ path: '/survey/tasks', name: 'Tasks', complete: false },
-		{ path: '/survey/sus', name: 'SUS', complete: false }
-	]);
-
-	const clockPoint = setDataStore('clockpoint', 'session', clockPointData);
-	const clockDraw = setDataStore('clockdraw', 'session', clockDrawData);
-	const wordRegistration = setDataStore('wordregistration', 'session', wordRegistrationData);
-	const wordRecall = setDataStore('wordrecall', 'session', wordRecallData);
-	const consent = setDataStore('consent', 'session', consentData);
-	const words = setDataStore('words', 'session', wordData);
-	const sus = setDataStore('sus', 'session', susData);
-	const router = setDataStore('router', 'session', routerData);
-
-	import CustomRouter from '../components/CustomRouter.svelte';
+	const context = setDataStore('data', 'session', allData);
 </script>
-
-<CustomRouter></CustomRouter>
 
 <div class="centered">
 	<Nav></Nav>
