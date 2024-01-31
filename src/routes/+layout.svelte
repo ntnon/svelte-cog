53923355578a71511b6fcf5a1cc2a9aa30b5a104
ntnon <Anton.nydal@hotmail.com>
2024-01-31 16:02:54 +0100

    use new components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
 	import { getAppData } from '$lib/dataService';
 	import { setDataStore } from '$lib/state.svelte';
-	import Nav from '../components/navigation/Nav.svelte';
+	import NavBar from '../components/NavBar.svelte';
+	import Progress from '../components/Progress.svelte';
 
 	const allData = getAppData();
 	const context = setDataStore(allData);
 </script>
 
 <div class="centered">
-	<Nav></Nav>
-	<slot />
+	<div class="slot">
+		<slot />
+	</div>
+
+	<Progress></Progress>
+	<NavBar debugMode={true} />
 </div>
 
 <!-- 
@@ -21,5 +26,15 @@
 		flex-direction: column;
 		height: 100vh;
 		align-items: center;
+	}
+
+	.slot {
+		justify-content: center;
+		padding-top: 10rem;
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+		align-items: center;
+		padding-bottom: 10rem;
 	}
 </style>
