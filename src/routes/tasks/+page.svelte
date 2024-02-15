2d0f523b4c62f83aeef11cbc06ecb11bfc50945a
ntnon <Anton.nydal@hotmail.com>
2024-02-15 14:10:37 +0100

    created slots

---
+++ src/routes/tasks/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import PageLayout from './wordregistration/PageLayout.svelte';
+</script>
+
+<PageLayout>
+	<div slot="header">This is the header content.</div>
+	<div slot="main">This is the main content.</div>
+	<div slot="footer">This is the footer content.</div>
+</PageLayout>
---
+++ src/routes/tasks/wordregistration/PageLayout.svelte
@@ -0,0 +1,13 @@
+<div class="container">
+	<header>
+		<slot name="header">headerfaølnback</slot>
+	</header>
+
+	<main>
+		<slot name="main">main fallnaclk</slot>
+	</main>
+
+	<footer>
+		<slot name="footer">footer fallback</slot>
+	</footer>
+</div>
