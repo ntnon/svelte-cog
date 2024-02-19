c024aefefdf8ada2904b7cea73ea498ebdb88127
ntnon <Anton.nydal@hotmail.com>
2024-02-19 14:02:31 +0100

    Remove NavBar component and update layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,28 +2,12 @@
 	import '../app.css';
 	import { getAppData } from '$lib/dataService';
 	import { setDataStore } from '$lib/state.svelte';
-	import NavBar from '../components/NavBar.svelte';
 
 	const allData = getAppData();
 	const context = setDataStore(allData);
 	let debug: boolean = false;
 </script>
 
-<button on:click={() => (debug = !debug)}>toggle debug mode</button>
-<div class="">
-	<div class="">
-		<NavBar {debug} />
-	</div>
-	<div class="">
-		<slot />
-	</div>
-</div>
-
-<!-- 
- -->
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-</style>
+<main class="w-[100vw] h-[100vh] flex justify-center">
+	<slot />
+</main>
