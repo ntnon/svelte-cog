0177054def14475b61544e476d0782b021948a7e
ntnon <Anton.nydal@hotmail.com>
2024-03-02 16:18:12 +0100

    Refactor generateClockMarkers function

--- src/scripts/generateClockMarkers.ts
+++ src/scripts/generateClockMarkers.ts
@@ -1,6 +1,6 @@
 import type { IMarker } from "$lib/interfaces"
 
-const fisherYates = (toShuffle: unknown[]) => {
+function fisherYates(toShuffle: unknown[]) {
     for (let i = (toShuffle.length - 1); i > 0; i -= 1) {
         const randomIndex = Math.floor(Math.random() * (i + 1));
         [toShuffle[i], toShuffle[randomIndex]] = [toShuffle[randomIndex], toShuffle[i]];
@@ -12,6 +12,8 @@ export const generateClockMarkers = (): IMarker[] => {
     const array = Array.from({ length: 12 }, (_, i) => {
         return {
             id: i + 1,
+            DOMElement: undefined,
+            initialDOMElement: undefined,
             x: 0,
             y: 0,
             angle: 0,
@@ -20,6 +22,8 @@ export const generateClockMarkers = (): IMarker[] => {
             isInsideClock: false,
         }
     })
+    const randomize = fisherYates(array) as IMarker[];
+    const split = randomize.splice(0, 6);
 
-    return fisherYates(array) as IMarker[];
+    return split;
 }
