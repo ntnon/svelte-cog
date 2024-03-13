77f1583ada499e9dc1f2be7e651aa687b41e4e83
ntnon <Anton.nydal@hotmail.com>
2024-03-13 10:03:22 +0100

    Update base path in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? 'https://ntnon.github.io/svelte-cog' : process.env.BASE_PATH
 		}
 	}
 };
