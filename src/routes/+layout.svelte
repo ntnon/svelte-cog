04813be533e6d8ab7b3f126a03ef263ea7ac41b2
ntnon <Anton.nydal@hotmail.com>
2024-03-13 11:36:10 +0100

    Update layout styling and remove unnecessary code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
 	import '../app.css';
 
-	import { browser } from '$app/environment';
-
 	import { setDataStore } from '$lib/state.svelte';
 	import { defaultAppData } from '$lib/defaultAppData';
 
 	setDataStore(defaultAppData);
-
-	if (browser) {
-		window.scrollTo(0, 1); // hide the address bar on mobile devices
-	}
 </script>
 
-<main class="w-[100vw] h-[100vh] flex justify-center text-2xl">
+<main class="fixed w-[100vw] h-[100vh] flex justify-center text-2xl">
 	<slot />
 </main>
 
