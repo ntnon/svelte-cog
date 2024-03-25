a2e91a1f6e84e2f0e404f2700eec14a9c94f590d
ntnon <Anton.nydal@hotmail.com>
2024-03-25 16:31:59 +0100

    removed empty lines

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import '../app.css';
-
 	import { setDataStore } from '$lib/state.svelte';
 	import { defaultAppData } from '$lib/defaultAppData';
-
 	setDataStore(defaultAppData);
 </script>
 
