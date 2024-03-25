fe4dc6625cde4af223492dfaf67a570b50263165
ntnon <Anton.nydal@hotmail.com>
2024-03-25 16:33:26 +0100

    Next button always trigger the animation to be true.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,15 @@
 	import ClockDraw from '../components/pages/ClockDraw.svelte';
 	import ClockHands from '../components/pages/ClockHands.svelte';
 	import Recall from '../components/pages/Recall.svelte';
+	import { getAppState } from '$lib/state.svelte';
 
 	const pages = [Introduction, Example, WordRegistration, ClockDraw, ClockHands, Recall];
 	let index = 0;
 
+	const animating = getAppState().isAnimating;
+
 	const fallbackFn = () => {
+		animating.set(true);
 		index = (index + 1) % pages.length;
 		page = pages[index];
 	};
@@ -17,6 +21,6 @@
 	let page = pages[index];
 </script>
 
-<svelte:component this={pages[index]} {fallbackFn}>
+<svelte:component this={page} {fallbackFn}>
 	{index} / 5
 </svelte:component>
