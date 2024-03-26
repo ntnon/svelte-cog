19400c8fe8b3bbbfc3e82cf8c403303186b4b30d
ntnon <Anton.nydal@hotmail.com>
2024-03-26 18:49:46 +0100

    moved safari mobile check to stage, which renders the components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,51 +3,20 @@
 	import { setDataStore } from '$lib/state.svelte';
 	import { defaultAppData } from '$lib/defaultAppData';
 	setDataStore(defaultAppData);
-	import { browser } from '$app/environment';
-
-	let browserName: string = '';
-	let userAgent: string = '';
-
-	if (browser) {
-		userAgent = navigator.userAgent;
-
-		if (userAgent.indexOf('Chrome') > -1) {
-			browserName = 'Chrome';
-		} else if (userAgent.indexOf('Firefox') > -1) {
-			browserName = 'Firefox';
-		} else if (userAgent.indexOf('MSIE ') > -1 || userAgent.indexOf('Trident/') > -1) {
-			browserName = 'Internet Explorer';
-		} else if (userAgent.indexOf('Edge') > -1) {
-			browserName = 'Edge';
-		} else if (userAgent.indexOf('Safari') > -1) {
-			browserName = 'Safari';
-		} else {
-			browserName = 'Other';
-		}
-	}
 </script>
 
-<meta content="viewport-fit=cover" />
-
-<svelte:head>
+<!-- <svelte:head>
 	<link rel="manifest" href="src/manifest.json" />
-</svelte:head>
+</svelte:head> -->
 
-{#if browserName}
-	<div class="z-[10] absolute">{userAgent}</div>
-	<main
-		class=" w-[100%] absolute text-lg md:text-4xl {browserName === 'Safari'
-			? 'h-[90%]'
-			: 'h-[100%]'}"
-	>
+<main>
+	<div class="text-lg md:text-4xl">
 		<slot />
-		<div class="h-5">ff</div>
-	</main>
-{/if}
+	</div>
+</main>
 
 <style>
 	main {
 		font-family: Verdana, Geneva, Tahoma, sans-serif;
-		padding-bottom: env(safe-area-inset-bottom);
 	}
 </style>
