6a98b24386bb83f860cc65b112dcb055e0a37ab0
ntnon <Anton.nydal@hotmail.com>
2024-04-16 16:08:53 +0200

    Introduction is now a landing page

--- src/components/pages/Introduction.svelte
+++ src/components/pages/Introduction.svelte
@@ -2,17 +2,20 @@
 	import Stage from '../Stage.svelte';
 	import { defaultNextLabel } from '$lib/constants';
 	import Button from '../Button.svelte';
+	import { getAppState } from '$lib/state.svelte';
+
+	let points = getAppState().points;
 
 	export let fallbackFn: () => void;
 </script>
 
 <Stage>
-	<span slot="name">Task name</span>
-	<span slot="info">Instructions</span>
-	<span slot="progress">Progress</span>
-	<span slot="component" class="size-full center">
-		Press<span class="inline-block">
-			<Button fn={fallbackFn} active={true}>{defaultNextLabel}</Button></span
-		> to continue
+	<span slot="name">Welcome to the online MCI test!</span>
+	<span slot="info">Complete the tasks, earn points. There are 6 unique tasks</span>
+	<span slot="component" class=" flex-col size-full center">
+		<p class="text-3xl center w-[40vw]">
+			The first task is to press the 'next' button when you are ready to move on!
+		</p>
 	</span>
+	<Button slot="next" fn={fallbackFn} active={true}>{defaultNextLabel}</Button>
 </Stage>
