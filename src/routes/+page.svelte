1bc537c7ce4bc69fdb0ea0d97198624cc0cfaa84
ntnon <Anton.nydal@hotmail.com>
2024-04-17 11:28:59 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
 	import Introduction from '../components/pages/Introduction.svelte';
 	import Example from '../components/pages/Example.svelte';
-	import WordRegistration from '../components/pages/WordRegistration.svelte';
+	import ItemRegistration from '../components/pages/ItemRegistration.svelte';
 	import ClockDraw from '../components/pages/ClockDraw.svelte';
 	import ClockHands from '../components/pages/ClockHands.svelte';
-	import Recall from '../components/pages/Recall.svelte';
+	import LongRecall from '../components/pages/LongRecall.svelte';
+	import ShortRecall from '../components/pages/ShortRecall.svelte';
 	import { getAppState } from '$lib/state.svelte';
-
-	const pages = [Introduction, Example, WordRegistration, ClockDraw, ClockHands, Recall];
+	let pageId = getAppState().pageIndex;
+	const pages = [
+		Introduction,
+		//Example, add back when ready
+		ItemRegistration,
+		ShortRecall,
+		ClockDraw,
+		ClockHands,
+		LongRecall
+	];
 	let index = 0;
 
 	const animating = getAppState().isAnimating;
@@ -21,6 +30,4 @@
 	let page = pages[index];
 </script>
 
-<svelte:component this={page} {fallbackFn}>
-	{index} / 5
-</svelte:component>
+<svelte:component this={pages[$pageId]} />
