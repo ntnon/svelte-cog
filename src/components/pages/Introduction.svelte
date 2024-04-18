819edcf5f099443fe5929b6fbc2d200fa5aec15d
ntnon <Anton.nydal@hotmail.com>
2024-04-18 14:01:09 +0200

    Update Introduction.svelte to improve cognitive health information and button label

--- src/components/pages/Introduction.svelte
+++ src/components/pages/Introduction.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
 	import Stage from '../Stage.svelte';
 	import NextButton from '../NextButton.svelte';
-	import { getAppState } from '$lib/state.svelte';
 </script>
 
 <Stage>
 	<span slot="name">Welcome to the online MCI test!</span>
 	<span slot="info">Complete the tasks, earn points. There are 6 unique tasks</span>
-	<span slot="component" class=" flex-col size-full center">
-		<p class="text-3xl center px-10">
-			The first task is to press the 'next' button when you are ready to move on!
+	<span slot="component" class=" size-full flex-col center space-y-10 text-xl p-16">
+		<p>
+			Mild Cognitive Impairment <i>can</i> lead to worsening cognitive health, but in most cases it does
+			not.
 		</p>
+		<p>The purpose of this website is to help you understand your cognitive health</p>
+		<p><b>NB: </b>This website cannot diagnose or improve your cognitive health you.</p>
+		<b>
+			If you are concerned about your cognitive health, please consult a healthcare professional.
+		</b>
+
+		<p>The first task is to press the 'next' button when you are ready to move on!</p>
 	</span>
-	<NextButton slot="next" />
+	<NextButton label="ok" slot="next" />
 </Stage>
