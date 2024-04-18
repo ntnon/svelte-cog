bf38e8b2184480d47996c6adb3e4c83c9ab56dac
ntnon <Anton.nydal@hotmail.com>
2024-04-18 14:00:59 +0200

    Update NextButton.svelte to use dynamic label prop

--- src/components/NextButton.svelte
+++ src/components/NextButton.svelte
@@ -3,6 +3,7 @@
 	import { getAppState } from '$lib/state.svelte';
 	const pageStore = getAppState().pageIndex;
 	export let active = true;
+	export let label: string = 'Next';
 </script>
 
-<NavButton {active} fn={() => pageStore.update((v) => v + 1)}>Next</NavButton>
+<NavButton {active} fn={() => pageStore.update((v) => v + 1)}>{label}</NavButton>
