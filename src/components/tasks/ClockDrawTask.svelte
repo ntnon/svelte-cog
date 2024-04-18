2d568db727e96de87f35fc17a5438b972e387880
ntnon <Anton.nydal@hotmail.com>
2024-04-18 14:00:46 +0200

    Fix bug in ClockDrawTask.svelte

--- src/components/tasks/ClockDrawTask.svelte
+++ src/components/tasks/ClockDrawTask.svelte
@@ -31,11 +31,13 @@
 	const updateMarker = (pos: IPos, m: IMarker) => {
 		if (!dialPos) return m;
 		let newAngle = calcAngle(dialPos, pos);
+		let pointsAt = (angleToClockHour(newAngle) + 6) % 12;
 		return {
 			...m,
 			calcDistFromMid: calcDistFromCenter(pos),
 			angle: newAngle,
-			pointsAt: (angleToClockHour(newAngle) + 6) % 12, //0.15 is a slight adjustment to a bug
+			pointsAt: pointsAt,
+			score: Math.abs(pointsAt - m.id),
 			completed: true
 		};
 	};
@@ -47,9 +49,13 @@
 	</Clock>
 	<span class="markers w-[100%] flex flex-row justify-center items-center"
 		>{#each markers as marker}
-			<Draggable on:positionChange={(e) => (marker = updateMarker(e.detail.newPosition, marker))}
-				><span class="size-[7vh] marker"
-					>{marker.id}_{(Math.round(marker.pointsAt * 100) / 100).toFixed(2)}</span
+			<Draggable
+				on:mouseUp={(e) =>
+					(marker = { ...updateMarker(e.detail.newPosition, marker), moving: false })}
+				on:mouseDown={() => (marker = { ...marker, moving: true })}
+				on:mouseDown={() => (marker = { ...marker, moving: true })}
+				><span class="size-[7vh] marker text-3xl {marker.moving ? 'scale-[175%] ' : ''}"
+					>{marker.id}</span
 				></Draggable
 			>
 		{/each}</span
