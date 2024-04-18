486b5e5807a563f377eb4c69c3947cde51c71270
ntnon <Anton.nydal@hotmail.com>
2024-04-18 14:01:33 +0200

    Add Results.svelte to page list in +page.svelte and removed unecessary code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,25 +6,20 @@
 	import ClockHands from '../components/pages/ClockHands.svelte';
 	import LongRecall from '../components/pages/LongRecall.svelte';
 	import ShortRecall from '../components/pages/ShortRecall.svelte';
+	import Results from '../components/pages/Results.svelte';
+
 	import { getAppState } from '$lib/state.svelte';
 	let pageId = getAppState().pageIndex;
 	const pages = [
 		Introduction,
-		//Example, add back when ready
+		//Example
 		ItemRegistration,
 		ShortRecall,
 		ClockDraw,
 		ClockHands,
-		LongRecall
+		LongRecall,
+		Results
 	];
-	let index = 0;
-
-	const fallbackFn = () => {
-		index = (index + 1) % pages.length;
-		page = pages[index];
-	};
-
-	let page = pages[index];
 </script>
 
 <svelte:component this={pages[$pageId % pages.length]} />
