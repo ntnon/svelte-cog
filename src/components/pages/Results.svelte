009c5ad1b687d8854110f4b4a1779db502d8023b
ntnon <Anton.nydal@hotmail.com>
2024-04-18 14:00:29 +0200

    Update Results.svelte

---
+++ src/components/pages/Results.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Stage from '../Stage.svelte';
+	import { getAppState } from '$lib/state.svelte';
+
+	const taskState = getAppState().taskData;
+	const keyValueArray = Object.entries(taskState);
+</script>
+
+<Stage>
+	<span slot="name">Hooray!!!</span>
+	<span slot="info" class="text-6xl">✨🎖️✨</span>
+
+	{#each keyValueArray as t}
+		<span class="text-6xl">{t}f</span>
+	{/each}
+</Stage>
