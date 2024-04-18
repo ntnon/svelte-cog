9b8785bbe6f8cf65e9b7d0bf4450815e3a51fc8b
ntnon <Anton.nydal@hotmail.com>
2024-04-18 10:20:00 +0200

    removed debugging code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,6 @@
 	import { defaultAppData } from '$lib/defaultAppData';
 	import { getAppState } from '$lib/state.svelte';
 	setDataStore(defaultAppData);
-
-	const appState = getAppState();
-	const longRecall = appState.taskData.longRecall;
-	const shortRecall = appState.taskData.shortRecall;
-	console.log($longRecall.data.correct);
-	console.log($shortRecall.data.correct);
 </script>
 
 <svelte:head>
