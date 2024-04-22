d65f7f406ec454b080a92fd450d9c5b8389594ea
ntnon <Anton.nydal@hotmail.com>
2024-04-22 16:21:29 +0200

    Create Jurrasic.svelte

---
+++ src/components/Jurrasic.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+</script>
+
+<div class="container scale-125 center flex flex-col">
+	<!-- Dinosaurs -->
+
+	<!-- Large Dinosaur -->
+	<span class="text-3xl ml-40 gap-10 space-y-10">
+		<span class="m-5 left-1/2">🌴</span>
+
+		<span class="m-1 py-20">🌴</span>
+	</span>
+
+	<span class="flex flex-row">
+		<span class="text-9xl">🌴</span>
+		<span class="text-6xl">🦕</span>
+		<span class="text-4xl">🦕</span>
+		<span class="text-4xl">🦕</span>
+	</span>
+
+	<!-- Medium Dinosaur -->
+</div>
