8c79a9ec3d96ca1ff81c4f9c1026035ca6407ce9
ntnon <Anton.nydal@hotmail.com>
2024-04-23 21:25:01 +0200

    Create characters.ts

---
+++ src/lib/characters.ts
@@ -0,0 +1,37 @@
+import type { ICharacter } from "./interfaces";
+
+export const narrator: ICharacter = {
+    name: 'Narrator',
+    char: '📖',
+    talkingSpeed: 1
+}
+
+export const neighbor: ICharacter = {
+    name: 'Neighbor',
+    char: '🙋🏼‍♂️🍨',
+    talkingSpeed: 15,
+};
+
+export const wizard: ICharacter = {
+    name: 'Wizard',
+    char: '🧙🏻‍♂️Wizard',
+    talkingSpeed: 1,
+};
+
+export const guy = {
+    name: 'Guy',
+    char: '🤵🏾‍♂️Guy',
+    talkingSpeed: 1
+};
+
+export const doctor = {
+    name: 'Doctor',
+    char: '👩🏽‍⚕️Doctor',
+    talkingSpeed: 1
+};
+
+export const scientist = {
+    name: 'Scientist',
+    char: '🧑🏼‍🔬Scientist',
+    talkingSpeed: 1
+}
