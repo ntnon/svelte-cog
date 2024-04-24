44c7a9b2a88a6683adc07d7be00e6169f5b3ddf1
ntnon <Anton.nydal@hotmail.com>
2024-04-24 21:11:01 +0200

    Update Reward.svelte

--- src/components/Reward.svelte
+++ src/components/Reward.svelte
@@ -13,7 +13,7 @@
 
 	export let options: IEmoji[] = [];
 
-	export let maxRewards: number = 1;
+	export let maxRewards: number = options.length;
 
 	$: rewards.update((v) => ({ ...v, maxRewards, options: options }));
 
@@ -27,13 +27,15 @@
 <div class="ease size-full center text-6xl flex flex-end nb">
 	{#each $rewards.options as item (item.name)}
 		<button
-			class="reward"
+			class="reward nb2"
 			animate:flip={{ duration: 300 }}
 			in:receive|global={{ key: item.name }}
 			out:send|global={{ key: item.name }}
 			on:click={() => {
 				rewards.select(item);
-				dispatch('complete', true);
+				if ($rewards.selected.length === $rewards.maxRewards) {
+					dispatch('complete', true);
+				}
 			}}
 			>{item.char}
 		</button>
