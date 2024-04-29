39bb61581bab34896d35a74392061f8075aef3ab
ntnon <Anton.nydal@hotmail.com>
2024-04-29 10:09:29 +0200

    Update Survey.svelte

--- src/components/pages/Survey.svelte
+++ src/components/pages/Survey.svelte
@@ -5,14 +5,15 @@
 </script>
 
 <div class="center flex-col space-y-10 h-[100dvh] top-1/2">
-	<p>Welcome to Web-Cog</p>
+	<b>Welcome to Web-Cog</b>
+	<p>play the game before doing the survey</p>
 	<button
 		class="center hover p-5 w-[50dvw] ease btnClass bg-green-500 border-5 border-solid rounded-full box-border border-green-500"
-		on:click={() => pageStore.update((v) => v + 1)}>Game</button
+		on:click={() => pageStore.update((v) => v + 1)}>1. Game</button
 	>
 	<a
 		class="center hover p-5 w-[50dvw] ease btnClass bg-green-500 border-5 border-solid rounded-full box-border border-green-500"
-		href="https://svar.uib.no/LinkCollector?key=TJSL3WGQLN16">Survey</a
+		href="https://svar.uib.no/LinkCollector?key=TJSL3WGQLN16">2. Survey</a
 	>
 </div>
 
