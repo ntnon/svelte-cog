f49d1e732a11a83cdf3305288e765de47a422bbb
ntnon <Anton.nydal@hotmail.com>
2024-04-29 10:05:29 +0200

    updated qr page

--- src/routes/qr/+page.svelte
+++ src/routes/qr/+page.svelte
@@ -3,19 +3,18 @@
 	import nydonpng from '$lib/qrpng.png';
 </script>
 
-<div class="center flex flex-col space-y-5">
+<div class="center flex flex-col space-y-5 bg-white">
 	<span class="center w-max">⭐⭐⭐⭐⭐⭐<span class="text-[8vmin]">🌟🌟🌟</span>⭐⭐⭐⭐⭐⭐</span>
+	<b>Anton sitt masterprosjekt, UiB</b>
 	<div class="flex flex-row center space-x-8 flex-wrap">
 		<div class="space-y-4">
-			<b>Anton sitt masterprosjekt</b>
 			<img class=" w-[60vmin] h-[60vmin] margin-inline-auto" src={nydonpng} alt={nydonsvg} />
 		</div>
 		<div class="flex flex-col">
 			<ul class="space-y-4">
 				<li>Spill spillet</li>
-				<li>Også er det undersøkelse</li>
-				<li>Det tar kort tid!</li>
-				<li>Og hjelper masse😊</li>
+				<li>Ta undersøkelsen (5min)</li>
+				<li>Takk!</li>
 				<li><u>https://www.nydon.no</u></li>
 			</ul>
 		</div>
